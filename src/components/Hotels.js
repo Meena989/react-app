54c680287fd24f8d2b3e32d9900e16f1606ef786
Meena989 <58164735+Meena989@users.noreply.github.com>
2021-09-22 20:35:45 +0530

    Update Hotels.js

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React from 'react';
+import img7 from '../assets/img7.jpeg'
+import img8 from '../assets/img8.jpeg'
 
-function Hotels () {
-    const style1={width:45,height:25,color:'white'}
-    const style2={width:25,height:15,color:'white'}
+function Hotels(){
+    const imgStyle={
+    padding:"10px",
+    left:"400px",
+    width:"30%",
+    height:"100%",
+    float:"left",
+    borderRadius:"5px"
     
-    return (
+    }
+    const cardStyle={
+        width:"200px",
+        height:"70px",
+        marginLeft:"200px",
+        marginTop:"500px",
+        float:"left"
+    }
+    const descrypt={
+        position:"absolute",
+        marginLeft:"70px",
+        float:"left"
+
+    }
+    return(
         <div>
-            <h2> Top Hotels </h2>
+            <h4 style={{marginTop:"50px"}}>Top Hotels</h4>
+            <div className="card" style={cardStyle}>
+                <img  style={imgStyle}src={img7} alt=" loading"/>
+                <div style={descrypt}>The golden Hotel<div>*&nbsp;<i className="fas fa-star"></i><b>9.0</b></div></div>
+            </div>
+            <div className="card" style={cardStyle}>
+                <img  style={imgStyle}src={img8} alt=" loading"/>
+                <div style={descrypt}>The Mount Hotel<div>&nbsp;<i className="fas fa-star"></i><b>8.7</b></div></div>
+            </div>
         </div>
     )
 }
-
 export default Hotels;
